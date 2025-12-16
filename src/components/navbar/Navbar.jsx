538ae103b9558eecd16b653f0cc71f103ef5407a
Leandro Pereira da Silva <leanpsilva@gmail.com>
2025-12-16 02:25:22 -0300

    feat: update navbar logo to use new SVG and remove old PNG file

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import SearchInput from '../search-input/SearchInput'
-import logo from '/src/assets/images/logo.png';
+import logo from '/src/assets/images/Arch_Amazon-Personalize_64.svg';  
 import './navbar.css'
 
 export default function Navbar() {
     return (
         <div className="navbar">
             <div className="navbar__nav-brand">
-                <img src={logo} className="w-16" alt="" />
-                <h2 className="text-2xl max-sm:hidden bg-gradient-to-r from-[#6889e6] via-[#a5bdff] to-[#d6e1ff] bg-clip-text text-transparent">Ronyzs</h2>
+                <img src={logo} className="w-16" alt="Personalize AWS logo" />
+                <h2 className="text-2xl max-sm:hidden bg-gradient-to-r from-[#6889e6] via-[#a5bdff] to-[#d6e1ff] bg-clip-text text-transparent">POC AWS Personalize</h2>
             </div>
             <SearchInput />
         </div>
