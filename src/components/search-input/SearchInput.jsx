0a046ad83d8ca27122dc1f849594612b18fe1ed0
Leandro Pereira da Silva <leanpsilva@gmail.com>
2026-01-05 10:57:51 -0300

    update users

--- src/components/search-input/SearchInput.jsx
+++ src/components/search-input/SearchInput.jsx
@@ -63,6 +63,8 @@ export default function SearchInput() {
                     <option value="75">User 75</option>
                     <option value="76">User 76</option>
                     <option value="111">User 111</option>
+                    <option value="214">User 214</option>
+                    <option value="245">User 245</option>
                     <option value="1000">User 1000</option>
                 </select>
             </div>
