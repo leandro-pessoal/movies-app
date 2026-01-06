1376d7bb6e56cfdea4e542c1564c6c3a367775c0
Leandro Pereira da Silva <leanpsilva@gmail.com>
2026-01-06 16:05:02 -0300

    update color ic play

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import SearchInput from '../search-input/SearchInput'
-import logo from '/src/assets/images/Arch_Amazon-Personalize_64.svg';  
+import logo from '/src/assets/images/symbol-ic-play.webp';  
 import './navbar.css'
 
 export default function Navbar() {
     return (
         <div className="navbar">
             <div className="navbar__nav-brand">
-                <img src={logo} className="w-16" alt="Personalize AWS logo" />
-                <h2 className="text-2xl max-sm:hidden bg-gradient-to-r from-[#6889e6] via-[#a5bdff] to-[#d6e1ff] bg-clip-text text-transparent">POC AWS Personalize</h2>
+                <img src={logo} className="w-12 mr-4"  />
+                <h2 className="text-4xl max-sm:hidden text-ic-play">Meu streaming</h2>
             </div>
             <SearchInput />
         </div>
